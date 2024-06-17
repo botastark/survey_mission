3b552c12c1113e898b152f5e069bb87e365e36bd
botastark <bota.duisenbay@gmail.com>
2024-06-17 17:10:44 +0200

    open/release camera in cpp

--- src/disable_DCM_offboard.py
+++ src/disable_DCM_offboard.py
@@ -4,33 +4,51 @@ import rospy
 from mavros_msgs.msg import State
 import subprocess
 
-# Global variable to track the previous state
+# Global variable to track the image_capture_node process
+image_capture_process = None
 previous_mode = ""
 
 def state_callback(state_msg):
-    global previous_mode
+    global previous_mode, image_capture_process
 
-    # Retrieve the current mode from the message
     current_mode = state_msg.mode
 
-    # Check if the mode has changed
     if current_mode != previous_mode:
         rospy.loginfo("Mode changed to {}".format(current_mode))
 
-
-        # Perform actions based on the current mode
         if current_mode == "OFFBOARD":
             rospy.loginfo("Disabling drone camera manager...")
             stop_drone_manager()
+            start_image_capture_node()
+
         else:
             rospy.loginfo("Enabling drone camera manager...")
+            stop_image_capture_node()
             start_drone_manager()
 
-        # Update previous_mode to the current mode
         previous_mode = current_mode
 
+def start_image_capture_node():
+    global image_capture_process
+    if image_capture_process is None:
+        try:
+            image_capture_process = subprocess.Popen(["rosrun", "survey_mission", "image_capture_node"])
+            rospy.loginfo("image_capture_node started.")
+        except Exception as e:
+            rospy.logerr("Error starting image_capture_node: {}".format(str(e)))
+
+def stop_image_capture_node():
+    global image_capture_process
+    if image_capture_process is not None:
+        try:
+            image_capture_process.terminate()
+            rospy.loginfo("image_capture_node stopped.")
+        except Exception as e:
+            rospy.logerr("Error stopping image_capture_node: {}".format(str(e)))
+        finally:
+            image_capture_process = None
+
 def stop_drone_manager():
-    # Use subprocess to run the command to stop drone_manager.service
     try:
         subprocess.call(["sudo", "systemctl", "stop", "dronecode-camera-manager.service"])
         rospy.loginfo("drone_manager.service stopped successfully.")
@@ -38,7 +56,6 @@ def stop_drone_manager():
         rospy.logerr("Error stopping drone_manager.service: {}".format(str(e)))
 
 def start_drone_manager():
-    # Use subprocess to run the command to start drone_manager.service
     try:
         subprocess.call(["sudo", "systemctl", "start", "dronecode-camera-manager.service"])
         rospy.loginfo("drone_manager.service started successfully.")
@@ -48,7 +65,6 @@ def start_drone_manager():
 def main():
     rospy.init_node('disable_dcm_node', anonymous=True)
     
-    # Subscribe to mavros/state topic
     rospy.Subscriber("/mavros/state", State, state_callback)
     
     rospy.spin()
--- src/image_capture.cpp
+++ src/image_capture.cpp
@@ -1,7 +1,9 @@
 #include <ros/ros.h>
 #include <std_msgs/Bool.h>
 #include <opencv2/opencv.hpp>
-
+#include <chrono>
+#include <ctime>
+#include <sys/stat.h>
 std::string gstreamer_pipeline(int sensor_id = 0,
                                int capture_width = 1920,
                                int capture_height = 1080,
@@ -18,19 +20,57 @@ std::string gstreamer_pipeline(int sensor_id = 0,
 }
 
 cv::VideoCapture video_capture;
-std::string base_path = "/home/uvify/Desktop/survey/";
-std::string original_filename = "captured_image";
+std::string base_path = "/home/uvify/Desktop/offboard_images/";
 
+
+std::string getCurrentDateTime() {
+    auto now = std::chrono::system_clock::now();
+    auto ms = std::chrono::time_point_cast<std::chrono::milliseconds>(now);
+    auto fractional_seconds = now - ms;
+
+    std::time_t now_c = std::chrono::system_clock::to_time_t(now);
+    std::tm now_tm = *std::localtime(&now_c);
+
+    char buffer[80];
+    strftime(buffer, sizeof(buffer), "%H-%M-%S", &now_tm);
+    std::stringstream ss;
+    ss << buffer << '-' << std::setfill('0') << std::setw(3) << fractional_seconds.count();
+    return ss.str();
+}
+
+void ensureDirectoryExists(const std::string& path) {
+    struct stat info;
+    if (stat(path.c_str(), &info) != 0) {
+        // Directory does not exist, create it
+        if (mkdir(path.c_str(), 0777) != 0) {
+            ROS_ERROR("Failed to create directory: %s", path.c_str());
+        }
+    } else if (!(info.st_mode & S_IFDIR)) {
+        ROS_ERROR("Path exists but is not a directory: %s", path.c_str());
+    }
+}
 void saveImageCallback(const std_msgs::Bool::ConstPtr& msg) {
     if (msg->data) {
         cv::Mat frame;
         video_capture >> frame;
         if (!frame.empty()) {
+	    std::string dateFolder = base_path;
 
-            ros::Time timestamp = ros::Time::now();
-            std::stringstream ss;
-            ss << timestamp.sec << "_" << timestamp.nsec;
-            std::string filename = base_path + original_filename + "_" + ss.str() + ".jpg";
+            // Create directory for current date if it does not exist
+            std::time_t rawtime = std::time(nullptr);
+            struct std::tm* timeinfo = std::localtime(&rawtime);
+            char buffer[80];
+            strftime(buffer, sizeof(buffer), "%Y-%m-%d", timeinfo);
+            dateFolder += buffer;
+
+            // Check if directory exists, create if not
+            ensureDirectoryExists(dateFolder);
+            std::string filename = dateFolder + "/" + getCurrentDateTime() + ".jpg";
+            
+            //ros::Time timestamp = ros::Time::now();
+            // std::stringstream ss;
+            //ss << timestamp.sec << "_" << timestamp.nsec;
+            //std::string filename = base_path + original_filename + "_" + ss.str() + ".jpg";
             
             cv::imwrite(filename, frame);
             // ROS_INFO("Image captured: %s", filename.c_str());
@@ -56,14 +96,11 @@ int main(int argc, char** argv) {
     while (ros::ok()) {
     
         video_capture >> frame;
-    
         ros::spinOnce();
     }
 
     ros::spin();
     video_capture.release();
 
-
     return 0;
 }
-
