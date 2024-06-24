543867c11506f001694b91b7b97f59523500a23c
Bota Duisenbay <33381341+botastark@users.noreply.github.com>
2024-06-25 00:34:07 +0200

    Update receiver.py

--- src/receiver.py
+++ src/receiver.py
@@ -1,17 +1,31 @@
 import socket
+import os
 def receive_file(save_as, port):
-
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 	s.bind(('', port)) 
 	s.listen(1) 
 	print("Listening on port {}...".format(port)) 
-	conn, addr = s.accept() 
-	print("Connection from {}".format(addr)) # Receive the data 
-	file_data = conn.recv(1024).decode('utf-8') # Save the received data to a file 
-	with open(save_as, 'w') as file: 
-		file.write(file_data) # Close the connection 
-	conn.close() 
+	while True: #indef-ly wait for updates
+		conn, addr = s.accept() 
+		print("Connection from {}".format(addr)) # Receive the data 
+	
+		file_data = []
+		while True: # Save the received data to a file 
+		    data = conn.recv(1024)
+		    if not data:
+			break
+		    file_data.append(data)
+		
+		file_data = b''.join(file_data).decode('utf-8')
+		# Check if the file already exists
+		if os.path.exists(save_as):
+			print(f"File {save_as} already exists. It will be replaced.")
+		with open(save_as, 'w') as file: 
+			file.write(file_data)
+		conn.close()
+		print(f"File {save_as} received and saved. Waiting for the next update.")
+	
 if __name__ == "__main__": 
-	SAVE_AS = '/home/uvify/received_file.txt' 
+	SAVE_AS = '/home/uvify/catkin_ws/src/survey_mission/path/waypoints.txt' 
 	PORT = 12345 # This should match the port used by the sender 
 	receive_file(SAVE_AS, PORT)
