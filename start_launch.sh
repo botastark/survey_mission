9a1022f47d1f6d00403056954bd0796384018f8e
Bota Duisenbay <33381341+botastark@users.noreply.github.com>
2024-06-25 01:06:51 +0200

    Update start_launch.sh

--- start_launch.sh
+++ start_launch.sh
@@ -7,6 +7,9 @@ roscore &
 ROSCORE_PID=$!
 sleep 5
 
-roslaunch survey_mission survey_camera.launch
+roslaunch survey_mission survey_camera.launch &
+
+# Start the receive_file.py script in the background
+python /home/uvify/catkin_ws/src/survey_mission/src/receive_file.py &
 
 wait $ROSCORE_PID
